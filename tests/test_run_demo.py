0504a91328ebefb1d0018a9a84afdc877119b95a
rob-graham <info@graham.id.au>
2025-09-04 15:23:21 +0930

    Include track edges in results output

--- tests/test_run_demo.py
+++ tests/test_run_demo.py
@@ -33,17 +33,8 @@ def test_one_corner_track_open_track() -> None:
     assert not np.allclose([x[0], y[0]], [x[-1], y[-1]])
     length = geom["s_m"].iloc[-1]
     assert np.isclose(length, 327.0, atol=1e-6)
-    for col in ["x_inner_m", "y_inner_m", "x_outer_m", "y_outer_m"]:
+    for col in ["x_left_m", "y_left_m", "x_right_m", "y_right_m"]:
         assert col in geom.columns
-    idx = np.where(geom["curvature_1pm"].to_numpy() != 0)[0][0]
-    curvature = geom["curvature_1pm"].iloc[idx]
-    inner = geom.loc[idx, ["x_inner_m", "y_inner_m"]].to_numpy()
-    outer = geom.loc[idx, ["x_outer_m", "y_outer_m"]].to_numpy()
-    left = geom.loc[idx, ["x_left_m", "y_left_m"]].to_numpy()
-    right = geom.loc[idx, ["x_right_m", "y_right_m"]].to_numpy()
-    if curvature > 0:
-        assert np.allclose(inner, left)
-        assert np.allclose(outer, right)
-    else:
-        assert np.allclose(inner, right)
-        assert np.allclose(outer, left)
+    results = pd.read_csv(out_dir / "results.csv")
+    for col in ["x_left_m", "y_left_m", "x_right_m", "y_right_m"]:
+        assert col in results.columns
